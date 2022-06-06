71205e3867ea65eb6932f3946af5ed8224fa6fe6
dphoeng <329163@student.mboutrecht.nl>
2022-06-06 21:48:04 +0200

    Fixed bug in content.php where users could visit nonexistent pages

--- components/content.php
+++ components/content.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["id"])) {
 }
 if (isset($_GET["content"])) {
   $url = explode("/", $_GET['content']);
-  if (file_exists("./content/" . $url[0]) . ".php") {
+  if (file_exists("./content/" . $url[0] . ".php")) {
     include("./content/" . $url[0] . ".php");
   }
 } else {
